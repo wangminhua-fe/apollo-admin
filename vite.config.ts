ab9f28765b6a09b93d32cbd66f34a1a4e3a653f5
peanut-dev <wmhayt228@gmail.com>
2022-03-19 11:43:12 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  //启动服务配置
+  server: {
+    host: '0.0.0.0',
+    port: 8000,
+    open: true,
+    https: false,
+    proxy: {},
+  },
+  // 生产环境打包配置
+  //去除 console debugger
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 })
